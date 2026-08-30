40688ecf4730f9e4cf702a3510197f5c2af85014
AHMED QAID <ahmed0qaid@gmail.com>
2026-08-30 13:15:08 +0300

    Add validator package

---
+++ src/n8n_production_workflows/__init__.py
@@ -0,0 +1,8 @@
+from .validator import ValidationIssue, ValidationReport, validate_catalog, validate_package
+
+__all__ = [
+    "ValidationIssue",
+    "ValidationReport",
+    "validate_catalog",
+    "validate_package",
+]
