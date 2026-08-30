b1429b0f1c00cd58d65783846f22fabe44739f6f
AHMED QAID <ahmed0qaid@gmail.com>
2026-08-30 17:56:50 +0300

    Export n8n v0.3 harness APIs

--- src/n8n_production_workflows/__init__.py
+++ src/n8n_production_workflows/__init__.py
@@ -1,13 +1,31 @@
 from .analyzer import NodeRisk, SemanticFinding, analyze_workflow, risk_summary
+from .harness import (
+    ExecutionSnapshot,
+    HarnessError,
+    TestResult,
+    WorkflowHarness,
+    WorkflowTestCase,
+    render_markdown,
+    run_catalog,
+    run_package,
+)
 from .validator import ValidationIssue, ValidationReport, validate_catalog, validate_package
 
 __all__ = [
+    "ExecutionSnapshot",
+    "HarnessError",
     "NodeRisk",
     "SemanticFinding",
+    "TestResult",
     "ValidationIssue",
     "ValidationReport",
+    "WorkflowHarness",
+    "WorkflowTestCase",
     "analyze_workflow",
+    "render_markdown",
     "risk_summary",
+    "run_catalog",
+    "run_package",
     "validate_catalog",
     "validate_package",
 ]
